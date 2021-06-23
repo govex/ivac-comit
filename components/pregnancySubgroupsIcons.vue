74273f95dfcc3bee226a9c8116df9ee33eb0055a
Andrew Nicklin <github@nicklin.info>
2021-06-23 13:24:32 -0400

    update hover text for subgroups icons icons

--- components/pregnancySubgroupsIcons.vue
+++ components/pregnancySubgroupsIcons.vue
@@ -25,7 +25,7 @@
           <path d="M82.566,17.057c-8.994-8.993-23.572-8.991-32.563,0l-2.503,2.503l-2.504-2.503c-8.991-8.991-23.569-8.993-32.562,0  c-8.993,8.992-8.993,23.572-0.002,32.563l35.067,35.067L82.566,49.62C91.557,40.629,91.557,26.049,82.566,17.057z M68.344,53.026  H53.271V68.1h-12.05V53.026H26.148v-12.05h15.072V25.903h12.05v15.073h15.073V53.026z" />
         </svg>
         <svg
-          v-if="code.value === '2nd and 3rd trimester only'"
+          v-if="code.value === 'Advises administration in the 2nd or 3rd trimester'"
           :key="code.value"
           v-b-popover.hover.top="code.value"
           xmlns="http://www.w3.org/2000/svg"
