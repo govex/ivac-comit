4d3e3f6508ca81fd85ba8f05d8dd597debbf9a0a
Andrew Nicklin <github@nicklin.info>
2021-06-23 14:01:43 -0400

    environment variable controls 'beta' badge

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <b-navbar-brand to="/">
           <img height="75px" src="/img/comit-dark-background.png" alt="COMIT LOGO">
-          <sup><b-badge variant="warning">beta</b-badge></sup>
+          <sup><b-badge v-if="!production" variant="warning">beta</b-badge></sup>
         </b-navbar-brand>
         <b-navbar-toggle target="nav-collapse" />
         <b-collapse id="nav-collapse" is-nav>
@@ -96,3 +96,13 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      production: process.env.NODE_ENV === 'production'
+    }
+  }
+}
+</script>
