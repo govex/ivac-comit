5bbbb1525190003b05814be6f9d6c1dd1e27f5ec
Andrew Nicklin <github@nicklin.info>
2021-08-11 10:59:11 -0400

    wording change to filter labels

--- components/pregnancyLactationFilter.vue
+++ components/pregnancyLactationFilter.vue
@@ -144,9 +144,9 @@ export default {
     filterText () {
       if (this.selectedVaccine === 'all') {
         if (this.selectedPolicyPositions.length === this.policyPositions.length) {
-          return `Showing the most permissive policy position ${this.timeWarpText} for each country for all vaccines`
+          return `Showing the most permissive policy position ${this.timeWarpText} for each country for any vaccines`
         } else {
-          return `Showing countries where the most permissive policy position ${this.timeWarpText} for all vaccines is ` + this.selectedPolicyPositions
+          return `Showing countries where the most permissive policy position ${this.timeWarpText} for any vaccines is ` + this.selectedPolicyPositions
             .map((selectedPolicyPosition) => {
               const policyPosition = this.policyPositions
                 .find(policyPosition => policyPosition.value === selectedPolicyPosition)
