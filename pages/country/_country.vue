be5dc7d17c6c0febb0271f6af066d36b3a035167
Andrew Nicklin <github@nicklin.info>
2021-12-01 15:48:56 -0500

    require date to not be wrapped

--- pages/country/_country.vue
+++ pages/country/_country.vue
@@ -140,7 +140,7 @@
                     <b-link v-if="authority.website3" v-b-popover.hover="'View website in a new browser tab'" :href="authority.website3" target="_blank"><b-icon-globe /></b-link>
                   </span>
                 </div>
-                <span v-if="authority.reviewEvents">Most recently reviewed by us on {{ authority.reviewEvents.slice(-1)[0] }}</span>
+                <span v-if="authority.reviewEvents">Most recently reviewed by us on <span style="white-space: nowrap">{{ authority.reviewEvents.slice(-1)[0] }}</span></span>
               </div>
               <h4 class="mt-4">{{ authorityByType.vaccinePositionsLabel }}</h4>
               <VaccinePositions :authority="authority">
