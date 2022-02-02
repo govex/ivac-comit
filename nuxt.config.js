fedf5507f383207732c0ad65b338ee664df2f6fa
Andrew Nicklin <github@nicklin.info>
2022-02-02 17:03:23 -0500

    revert devtools flag in build

--- nuxt.config.js
+++ nuxt.config.js
@@ -160,9 +160,6 @@ export default {
   ],
 
   publicRuntimeConfig: {
-    build: {
-      devtools: process.env.HIDE_BETA_BADGE !== 'true'
-    },
     googleAnalytics: {
       id: process.env.GOOGLE_GTAG_ID
     },
