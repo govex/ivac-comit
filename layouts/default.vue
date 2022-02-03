33f0d34f5915bc203b750a1e00b4417d2295e9d6
Andrew Nicklin <github@nicklin.info>
2022-02-02 23:24:51 -0500

    remove mounted event

--- layouts/default.vue
+++ layouts/default.vue
@@ -135,9 +135,6 @@ export default {
     await this.$store.commit('authorities/load', myReconstructedData.authorities)
     await this.$store.commit('policies/load', myReconstructedData.policies)
     await this.$store.commit('vaccines/load', myReconstructedData.vaccines)
-  },
-  mounted () {
-    console.log(`page updated, title: ${this.head?.title}, url: ${this.$route.path}`)
   }
 }
 </script>
